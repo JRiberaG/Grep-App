1b3422f391405d8cd00e38104c76db98a5d59591
JRiberaG <51130683+JRiberaG@users.noreply.github.com>
2020-05-27 01:39:56 +0200

    Layout created and some of the app already made

---
+++ src/support/StringHelper.java
@@ -0,0 +1,9 @@
+package support;
+
+public class StringHelper {
+    // <editor-fold defaultstate="collapsed" desc="LABEL ERRORS">
+    public static String errorPath404 = "This path / file does not exists";
+    public static String errorPathEmpty = "This field can't be empty";
+    public static String errorPatternEmpty = "This field can't be empty";
+    // </editor-fold>
+}
